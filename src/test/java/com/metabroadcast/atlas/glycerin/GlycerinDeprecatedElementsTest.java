afa5bd4815d22426b0fc9c62856be58e9be632ee
Martins Irbe <martins.git@irbe.co.uk>
2016-01-04 21:40:20 +0000

    MBST-13653: Added all current elements to the ignorable deprecated elements in the hashmultimap. Added stream for each feed type to check if the deprecated element list contains unknown element deprecation.

--- src/test/java/com/metabroadcast/atlas/glycerin/GlycerinDeprecatedElementsTest.java
+++ src/test/java/com/metabroadcast/atlas/glycerin/GlycerinDeprecatedElementsTest.java
@@ -1,105 +1,177 @@
 package com.metabroadcast.atlas.glycerin;
 
 import java.text.ParseException;
-import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.google.common.base.Optional;
-import com.google.common.net.HostSpecifier;
 import com.metabroadcast.atlas.glycerin.model.Feed;
 import com.metabroadcast.atlas.glycerin.model.Feeds;
 import com.metabroadcast.atlas.glycerin.queries.FeedsQuery;
 
+import com.beust.jcommander.internal.Lists;
+import com.google.common.base.Optional;
+import com.google.common.collect.HashMultimap;
+import com.google.common.net.HostSpecifier;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.fail;
+
 public class GlycerinDeprecatedElementsTest {
 
     private GlycerinHttpClient glycerinClient;
-    private HashMap<String, List<String>> ignorableDeprecations;
+    private HashMultimap<String, String> ignorableDeprecations;
+
+    @Test(groups = "integration")
+    private void testProgrammeForDeprecatedFields() throws GlycerinException {
+        GlycerinQuery<Feeds, Feed> query = new FeedsQuery();
+        GlycerinResponse<Feed> response = glycerinClient.get(query);
+        
+        List<String> deprecatedFields = response.getResults().stream().flatMap(feed -> {
+            String feedName = feed.getName();
+            return feed.getDeprecations().getDeprecated().stream()
+                    .flatMap(deprecatedElement -> {
+                        if (!ignorableDeprecations.containsEntry(
+                                feedName, deprecatedElement.getName()
+                        )) {
+                            return Stream.of(
+                                    String.format("%s : %s", feedName, deprecatedElement.getName())
+                            );
+                        } else {
+                            return Stream.empty();
+                        }
+                    });
+        }).collect(Collectors.toList());
+
+        if (!deprecatedFields.isEmpty()) {
+            String fields = "Deprecated fields: \n";
+            for (String field : deprecatedFields) {
+                fields = fields + field + "\n";
+            }
+            fail(fields);
+        }
+    }
 
     @Parameters({ "nitro.host", "nitro.apikey" })
     @BeforeClass(groups = "integration")
-    public void setup(String host, String apiKey) throws ParseException {
-//        List<String> programmesList = ImmutableList.copyOf(getProgrammesDeprecatedElements());
-//        List<String> availabilityList = ImmutableList.copyOf(getAvailabilityDeprecatedElements());
-//        List<String> broadcastsList = ImmutableList.copyOf(getBroadcastsDeprecatedElements());
-//        List<String> groupsList = ImmutableList.copyOf(getGroupsDeprecatedElements());
-//        List<String> imagesList = ImmutableList.copyOf(getImagesDeprecatedElements());
-//        List<String> masterbrandsList = ImmutableList.copyOf(getMasterbrandsDeprecatedElements());
-//        List<String> peopleList = ImmutableList.copyOf(getPeopleDeprecatedElements());
-//        List<String> pipsList = ImmutableList.copyOf(getPipsDeprecatedElements());
-//        List<String> promotionsList = ImmutableList.copyOf(getPromotionsDeprecatedElements());
-//        List<String> schedulesList = ImmutableList.copyOf(getSchedulesDeprecatedElements());
-//        List<String> servicesList = ImmutableList.copyOf(getServicesDeprecatedElements());
-//        List<String> versionsList = ImmutableList.copyOf(getVersionsDeprecatedElements());
+    public void setUp(String host, String apiKey) throws ParseException {
+        this.ignorableDeprecations = HashMultimap.create();
+        addDeprecatedElementsToTheIgnorableMap();
 
         glycerinClient = new GlycerinHttpClient(HostSpecifier.from(host), apiKey, null,
-                Optional.of("nitro/api"));
+                Optional.absent());
     }
 
-    @Test(groups = "integration")
-    public void testProgrammeForDeprecatedFields() throws GlycerinException {
-        GlycerinQuery<Feeds, Feed> query = new FeedsQuery();
-        GlycerinResponse<Feed> response = glycerinClient.get(query);
-
-        response.getResults().stream().forEach(feed -> {
-            feed.getName();
-        });
+    private void addDeprecatedElementsToTheIgnorableMap() {
+        addProgrammesDeprecatedElements();
+        addBroadcastsDeprecatedElements();
+        addGroupsDeprecatedElements();
+        addItemsDeprecatedElements();
+        addSchedulesDeprecatedElements();
+        addVersionsDeprecatedElements();
+        addAvailabilityDeprecatedElements();
+        addImagesDeprecatedElements();
+        addMasterbrandsDeprecatedElements();
+        addPeopleDeprecatedElements();
+        addPipsDeprecatedElements();
+        addPromotionsDeprecatedElements();
+        addServicesDeprecatedElements();
     }
-// TODO : Need to add all the deprecated elements for each feed type.
-//    public List<String> getProgrammesDeprecatedElements() {
-//        List<String> programmesDeprecatedElements = new ArrayList<>();
-//        programmesDeprecatedElements.add();
-//        programmesDeprecatedElements.add();
-//        programmesDeprecatedElements.add();
-//        programmesDeprecatedElements.add();
-//        programmesDeprecatedElements.add();
-//        programmesDeprecatedElements.add();
-//    }
-//
-//    public List<String> getAvailabilityDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getBroadcastsDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getGroupsDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getImagesDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getMasterbrandsDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getPeopleDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getPipsDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getPromotionsDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getSchedulesDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getServicesDeprecatedElements() {
-//
-//    }
-//
-//    public List<String> getVersionsDeprecatedElements() {
-//
-//    }
+
+    private void addProgrammesDeprecatedElements() {
+        List<String> programmesDeprecatedElements = Lists.newArrayList();
+        programmesDeprecatedElements.add("availability_from");
+        programmesDeprecatedElements.add("availability_to");
+        programmesDeprecatedElements.add("initial_letter_stop");
+        programmesDeprecatedElements.add("most_popular");
+        programmesDeprecatedElements.add("versions_availability");
+        programmesDeprecatedElements.add("people");
+        programmesDeprecatedElements.add("titles");
+        programmesDeprecatedElements.add("genre_groups");
+        programmesDeprecatedElements.add("n:formats");
+        programmesDeprecatedElements.add("n:genre_groups");
+        programmesDeprecatedElements.add("n:image");
+
+        ignorableDeprecations.putAll("Programmes", programmesDeprecatedElements);
+    }
+
+    private void addBroadcastsDeprecatedElements() {
+        List<String> broadcastDeprecatedElements = Lists.newArrayList();
+        broadcastDeprecatedElements.add("n:image");
+
+        ignorableDeprecations.putAll("Broadcasts", broadcastDeprecatedElements);
+    }
+
+    private void addGroupsDeprecatedElements() {
+        List<String> groupsDeprecatedElements = Lists.newArrayList();
+        groupsDeprecatedElements.add("n:image");
+
+        ignorableDeprecations.putAll("Groups", groupsDeprecatedElements);
+    }
+
+
+    private void addItemsDeprecatedElements() {
+        List<String> itemsDeprecatedElements = Lists.newArrayList();
+        itemsDeprecatedElements.add("offset");
+        itemsDeprecatedElements.add("n:image");
+
+        ignorableDeprecations.putAll("Items", itemsDeprecatedElements);
+    }
+
+    private void addSchedulesDeprecatedElements() {
+        List<String> schedulesDeprecatedElements = Lists.newArrayList();
+        schedulesDeprecatedElements.add("titles");
+        schedulesDeprecatedElements.add("n:image");
+
+        ignorableDeprecations.putAll("Schedules", schedulesDeprecatedElements);
+    }
+
+    private void addVersionsDeprecatedElements() {
+        List<String> versionsDeprecatedElements = Lists.newArrayList();
+        versionsDeprecatedElements.add("n:version_types");
+
+        ignorableDeprecations.putAll("Versions", versionsDeprecatedElements);
+    }
+
+    /*
+    * The following methods adds an empty list to the MultiHashMap because at the moment
+    * there are no ignorable deprecated fields for the specific feed types.
+    */
+
+    private void addAvailabilityDeprecatedElements() {
+        List<String> availabilityDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Availability", availabilityDeprecatedElements);
+    }
+
+    private void addImagesDeprecatedElements() {
+        List<String> imagesDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Images", imagesDeprecatedElements);
+    }
+
+    private void addMasterbrandsDeprecatedElements() {
+        List<String> masterbrandDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Masterbrands", masterbrandDeprecatedElements);
+    }
+
+    private void addPeopleDeprecatedElements() {
+        List<String> peopleDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("People", peopleDeprecatedElements);
+    }
+
+    private void addPipsDeprecatedElements() {
+        List<String> pipsDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Pips", pipsDeprecatedElements);
+    }
+
+    private void addPromotionsDeprecatedElements() {
+        List<String> promotionsDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Promotions", promotionsDeprecatedElements);
+    }
+
+    private void addServicesDeprecatedElements() {
+        List<String> servicesDeprecatedElements = Lists.newArrayList();
+        ignorableDeprecations.putAll("Services", servicesDeprecatedElements);
+    }
 }
